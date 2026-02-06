d485becc45b12328ee5a124d0857d46d17d5fdac
Shivam Garade <shivamgarade05@gmail.com>
2026-02-06 09:32:17 +0530

    frontend as it for now

---
+++ 10_Project_Moody_Day_04/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FacialExpression from './components/FacialExpression'
+
+function App() {
+
+  return (
+    <>
+      <FacialExpression />
+    </>
+  )
+}
+
+export default App
