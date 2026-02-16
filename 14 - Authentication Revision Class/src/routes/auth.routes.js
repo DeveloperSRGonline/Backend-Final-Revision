dc44fc0bb8f19f529bf1aee73aa3e846d7ea6b44
Shivam Garade <shivamgarade05@gmail.com>
2026-02-16 23:48:20 +0530

    stopped due to sleep

---
+++ 14 - Authentication Revision Class/src/routes/auth.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// post /register req.body = {username,password}
+router.post('/register',(req,res)=>{
+    const {username,password} = req.body;
+})
+
+module.exports = router;
