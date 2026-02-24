e8af4164de863fe901d3c01cbb4c8d1fa7d1dfbe
Shivam Garade <shivamgarade05@gmail.com>
2026-02-24 08:18:22 +0530

    Made some changes

--- 14 - Authentication JWT and token/server.js
+++ 14 - Authentication JWT and token/server.js
@@ -1,7 +1,15 @@
 require('dotenv').config()
+const express = require('express')
 const app = require('./src/app')
 
+const PORT = 8000
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000!')
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the server!' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
