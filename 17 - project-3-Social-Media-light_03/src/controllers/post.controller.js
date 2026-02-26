330c667b1b194ab0ec6257a5f7f20029d9ae40d0
Shivam Garade <shivamgarade05@gmail.com>
2026-02-27 00:54:14 +0530

    AI integration

--- 17 - project-3-Social-Media-light_03/src/controllers/post.controller.js
+++ 17 - project-3-Social-Media-light_03/src/controllers/post.controller.js
@@ -1,7 +1,22 @@
 const postModel = require("../models/post.model");
+const { generateCaption } = require("../services/ai.service");
 
 
 async function createPostController(req,res){
     const file = req.file;
+    // console.log('file received', file);
+
+    const base64Image = new Buffer.from(file.buffer).toString('base64');
+    // console.log('base64Image', base64Image);
+
+    const Caption = await generateCaption(base64Image);
+    // console.log('Caption', Caption);
     
+    res.json({
+        Caption
+    });
 }
+
+module.exports = {
+    createPostController
+};
--- 17 - project-3-Social-Media-light_03/src/routes/post.routes.js
+++ 17 - project-3-Social-Media-light_03/src/routes/post.routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth..middleware");
 const multer = require("multer");
+const {createPostController} = require("../controllers/post.controller");
 
 const uppload = multer({storage:multer.memoryStorage()})
 
 
 // POST /api/posts [protected] {image-file}
-router.post('/',authMiddleware)
+router.post('/',authMiddleware, uppload.single('image'), createPostController)
  
 module.exports = router;
